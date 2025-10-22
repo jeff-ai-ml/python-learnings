87b05f836ec982610f27d390d2490b4287f1762c
jeff-ai-ml <gakjefferson@gmail.com>
2025-10-22 23:46:39 +0530

    New code added for basic program

--- OOPS/basic.py
+++ OOPS/basic.py
@@ -15,13 +15,14 @@ class Owner:
         self.address = address
         self.contact = contact
 
+owner1 = Owner("Anu", "North road", 8823232)
 dog1 = Dog("Tommy", "German Sheapard")
 dog1.bark()
 print(f"Dog1 Name : {dog1.name}")
 print(f"Dog1 Breed :{dog1.breed}")
 dog1.bark()
  
-
+owner1 = Owner("Ban", "West road", 797797)
 dog2 = Dog("Puppy", "Hussky")
 dog2.bark()
 print(f"Dog2 Name : {dog2.name}")
