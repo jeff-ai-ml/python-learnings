c4f94a2ecf42baaa95d7bad2695077da5ea1e314
jeff-ai-ml <gakjefferson@gmail.com>
2025-11-01 21:39:56 +0530

    New program commits

--- OOPS/own_try_static_method.py
+++ OOPS/own_try_static_method.py
@@ -2,9 +2,7 @@
 
 class Vechile:
 
-    industry = "Transport"
-
-
+    industry = "Transport"  ## Static attribute defined
     
     def __init__(self, type, brand, name, model):
         self.type = type
@@ -17,6 +15,8 @@ class Vechile:
         print(f"Model of Vechile : {self.model}")
         print(f"Brand of Vechile : {self.brand}")
         print(f"Type of Vechile : {self.type}")
+        print(f"Road Tax : {Vechile.get_road_tax(self)}")    ## Static method used
+        print(f"Vechile Industry : {Vechile.industry}")   
 
         try:
             if self.color and self.millage:
@@ -28,16 +28,21 @@ class Vechile:
     
     def add_details(self,color,millage):
         self.color = color
-        self.millage = millage
+        self.millage = millage 
 
-        
+    @staticmethod                ## Static method defined
+    def get_road_tax(self):
+        if self.model <2010:
+            return 1500
+        else:
+            return 5000
+                
 
 v1 = Vechile("Car", "Toyota", "Etios", 2020)
 v2 = Vechile("Bike", "Yamaha", "FZ-16", 2011)
 
 v1.display_vechile()
 v2.display_vechile()
-
 v1.add_details("Yellow", "10KMPL")
 v1.display_vechile()
 v2.display_vechile()
